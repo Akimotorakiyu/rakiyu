515976e3ac161f8769fc8f706128921ea7f41456
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-08-30 02:16:14 +0800

    clear

--- packages/rakiyu/src/About.vue
+++ packages/rakiyu/src/About.vue
@@ -1,60 +1,11 @@
 <script lang="ts">
-import {
-  defineComponent,
-  getCurrentInstance,
-  onMounted,
-  watch,
-  computed,
-  watchEffect,
-  onBeforeUnmount,
-  reactive,
-} from "vue";
-// import { useStore } from "vuex";
-import store from "./store";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup(props) {
-    const instance = getCurrentInstance();
-    console.log("instance", instance);
-    // refs should use after instance created
-    // refs not work in here
-    console.log("refs", instance.refs);
-    onMounted(() => {
-      // refs work in here
-      console.log("refs", instance.refs);
-    });
-
-    // 取出挂载在app实例上的store
-    // const store1 = useStore();
-
-    //
-    computed(() => {});
-    watch([], () => {});
-
-    onBeforeUnmount(() => {});
-
-    return {
-      count_x: computed(() => store.state.count_x),
-      data: reactive([{ name: "Lilei" }, { name: "HanMeimei" }]),
-    };
-  },
+  setup(props) {},
 });
 </script>
 
-<template>
-  <h2>1</h2>
-  <MyTable :data="data">
-    <template #default="{type,row}">
-      <TableRow :row="row" :type="type" label="name" prop="name"></TableRow>
-      <TableRow :row="row" :type="type" label="name" prop="name"></TableRow>
-    </template>
-  </MyTable>
-  <h2>嵌套路由</h2>
-  <router-view></router-view>
-</template>
+<template>关于</template>
 
-<style lang="stylus" scoped>
-.green {
-  background-color: green;
-}
-</style>
+<style lang="stylus" scoped></style>
--- packages/rakiyu/src/App.vue
+++ packages/rakiyu/src/App.vue
@@ -1,24 +1,12 @@
 <script lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
 import { defineComponent } from "vue";
-
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
-  },
 });
 </script>
 
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3.0 + Vite" />
-    <router-link to="/">home</router-link>
-    <router-link to="/About">About</router-link>
-
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <style lang="stylus" scoped></style>
