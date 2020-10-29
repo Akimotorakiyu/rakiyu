251de402ecdf57a414df0ac609cd54ce717c0cb5
Cheng Ma <machengbeihang@gmail.com>
2020-10-29 12:48:45 +0800

    make header

---
+++ packages/rakiyu/src/views/Common1/editor/Header.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+import {
+  defineComponent,
+  onMounted,
+  getCurrentInstance,
+  reactive,
+  ref,
+  inject,
+  Component,
+  ComponentInternalInstance,
+  onUpdated,
+  defineAsyncComponent,
+} from "vue";
+import { nanoid } from "nanoid";
+import { EditorHub } from "./eventHub";
+import { EndNode, ContainerNode } from "./types";
+import TextNode from "./TextNode.vue";
+export default defineComponent({
+  components: { TextNode },
+  props: {
+    doc: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    parent: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  setup(props: { doc: EndNode; parent: ContainerNode }) {
+    let instance: ComponentInternalInstance;
+
+    const id = ref(nanoid());
+
+    let nodeElement: HTMLElement;
+
+    const editorEventHub = inject<EditorHub>("editorHub");
+
+    onMounted(() => {
+      instance = getCurrentInstance();
+      nodeElement = instance.refs?.nodeElement as HTMLElement;
+    });
+
+    function dealInput(params: Event) {
+      console.log("input", params);
+    }
+
+    return {
+      id,
+      methods: {
+        dealInput,
+      },
+    };
+  },
+});
+</script>
+
+<template>
+  <h1 :id="id" ref="nodeElement" :key="id">
+    <template v-for="(item, index) in doc.children" :key="index">
+      <TextNode :doc="item" :parent="doc" :index="index"></TextNode>
+    </template>
+  </h1>
+</template>
